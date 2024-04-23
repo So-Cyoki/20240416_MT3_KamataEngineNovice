dacfdadf4b014af9625912336f500c1750f6df42
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-04-23 11:18:45 +0900

    00_03 確認課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,27 @@
 #include <Novice.h>
+#include <cassert>
 
 const char kWindowTitle[] = "GC2A_07_ソウ_チョウキ_MT3";
+
+// 自分の型
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+// 画面にVector3とMatrix4x4を表示する
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 matrix, const char* label);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +32,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	// 自分の変数
+	Vector3 translate = {4.1f, 2.6f, 0.8f};
+	Vector3 scale = {1.5f, 5.2f, 7.3f};
+	Vector3 point = {2.3f, 3.8f, 1.4f};
+	Matrix4x4 transformMatrix = {1.f, 2.f, 3.f, 4.f, 3.f, 1.f, 1.f, 2.f, 1.f, 4.f, 2.f, 3.f, 2.f, 2.f, 1.f, 3.f};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +52,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 transformed = Transform(point, transformMatrix);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +63,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(10, 10, transformed, "Transformed");
+		MatrixScreenPrintf(10, kRowHeight * 1 + 10, translateMatrix, "TranslateMatrix");
+		MatrixScreenPrintf(10, kRowHeight * 6 + 10, scaleMatrix, "ScaleMatrix");
 
 		///
 		/// ↑描画処理ここまで
@@ -49,4 +84,53 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result = {0};
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[0][0] = result.m[1][1] = result.m[2][2] = result.m[3][3] = 1;
+	return result;
+}
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {0};
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1;
+	return result;
+}
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result = {0};
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.f * matrix.m[3][3];
+
+	assert(w != 0);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+}
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
 }
