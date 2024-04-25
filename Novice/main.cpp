1bc45ae9153041b419201003356eed9be7d8bab8
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-04-25 16:42:22 +0900

    01_00 確認課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,23 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "GC2A_07_ソウ_チョウキ_MT3";
+
+// 自分の型
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+// ビューポート変換行列
+Matrix4x4 MakeViewporMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
+const int kColumnWidth = 60;
+const int kRowHeight = 20;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 matrix, const char* label);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +28,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	// 自分の変数
+	Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.f, 160.f, 200.f, 300.f, 0.f, 1000.f);
+	Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.f);
+	Matrix4x4 viewporMatrix = MakeViewporMatrix(100.f, 200.f, 600.f, 300.f, 0.f, 1.f);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -33,6 +55,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		MatrixScreenPrintf(10, 10, orthographicMatrix, "OrthographicMatrix");
+		MatrixScreenPrintf(10, 10 + kRowHeight * 5, perspectiveFovMatrix, "PerspectiveFovMatrix");
+		MatrixScreenPrintf(10, 10 + kRowHeight * 10, viewporMatrix, "ViewporMatrix");
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -49,4 +75,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 result = {0};
+
+	result.m[0][0] = 1 / aspectRatio * cosf(fovY / 2) / sinf(fovY / 2);
+	result.m[1][1] = cosf(fovY / 2) / sinf(fovY / 2);
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+	result.m[2][3] = 1;
+	return result;
+}
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result = {0};
+	result.m[0][0] = 2 / (right - left);
+	result.m[1][1] = 2 / (top - bottom);
+	result.m[2][2] = 1 / (farClip - nearClip);
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	result.m[3][3] = 1;
+	return result;
+}
+
+Matrix4x4 MakeViewporMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result = {0};
+	result.m[0][0] = width / 2;
+	result.m[1][1] = -height / 2;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + width / 2;
+	result.m[3][1] = top + height / 2;
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1;
+	return result;
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
 }
